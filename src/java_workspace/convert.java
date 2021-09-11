f4003910b85db237df5fd61bfa11c089e475be67
shanm <shanm@Kaushik>
2021-09-11 15:37:22 +0530

    commit to eclipse

---
+++ src/java_workspace/convert.java
@@ -0,0 +1,15 @@
+package java_workspace;
+
+public class convert {
+
+	public static void main(String[] args) {
+		int a=120;
+		long b=a;
+		System.out.println(b);
+		long c=35;
+	   int d= (int)c;
+	   System.out.println(d);
+
+	}
+
+}
